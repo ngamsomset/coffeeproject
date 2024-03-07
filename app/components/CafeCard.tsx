9d1ddf13572cb511c5f335fac425c3ddedb3d2b2
sanderkg <sanderkvale@me.com>
2024-03-07 21:52:58 +1100

    Temporary list of cafes

--- app/components/CafeCard.tsx
+++ app/components/CafeCard.tsx
@@ -1,17 +1,19 @@
 "use client"
 import React from 'react'
 import { Card } from 'react-daisyui'
+import Link from 'next/link'
 
-const CafeCard = () => {
+const CafeCard = ({name, location, slug, imagePath} : {name:any, location:any, slug:any, imagePath:any}) => {
   return (
+    <Link href={slug}>
     <Card className='transition duration-500 hover:scale-105'>
-      <Card.Image src="/images/cafe_street.jpg" alt="Stock image of a cafe" />
+      <Card.Image src={imagePath} alt="Stock image of a cafe" />
       <Card.Body>
-        <Card.Title tag="h2">Verve</Card.Title>
-        <p>Location</p>
+        <Card.Title tag="h2">{name}</Card.Title>
+        <p>{location}</p>
       </Card.Body>
     </Card>
-
+    </Link>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import CafeCard from "./components/CafeCard";
 
+// Temporary list till we can connect to actual API requests with the ML model
+const cafeSuggestions = [
+  { name: 'Cafe 1', location: 'North Wollongong, NSW', slug: '#', imagePath: '/images/cafe_street.jpg'},
+  { name: 'Cafe 2', location: 'Bellambi, NSW', slug: '#', imagePath: '/images/cafe_street.jpg'},
+  { name: 'Cafe 3', location: 'Port Kembla, NSW', slug: '#', imagePath: '/images/cafe_street.jpg'},
+];
+
 export default async function Home() {
   return(
     <section className="mt-12 max-w-screen-lg mx-auto min-h-[60vh]">
       <h1 className="text-3xl font-semibold text-center mb-4 text-[#582F0E]">Top 3 Recommended Cafés for You</h1>
       <div className="flex gap-5">
-        <CafeCard/>
-        <CafeCard/>
-        <CafeCard/>
+        {cafeSuggestions.map((cafe, index) => (
+          <CafeCard name={cafe.name} location={cafe.location} slug={cafe.slug} imagePath={cafe.imagePath} key={index}/>
+        ))}
       </div>
     </section>
   );

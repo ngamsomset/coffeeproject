57d50b6d6422559ab717a84568dc64efaecd6285
Chutikarn Photharin <polyboty@polybotys-air.lan.uow.edu.au>
2024-03-14 17:07:58 +1100

    add signup form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <div>login</div>;
+
+import React from 'react'
+import LoginFormWithPicture from "../components/LoginFormWithPicture"
+
+
+const page = () => {
+  return (
+    <><section className="mt-12 max-w-screen-lg mx-auto ">
+      
+    </section><div className="container mx-auto p-4">
+        <LoginFormWithPicture />
+      </div></>
+  )
 }
+
+export default page
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,34 @@ const config: Config = {
     'node_modules/react-daisyui/dist/**/*.js',
   ],
   theme: {
+    colors:{
+      question:'#E6CCB2',
+      button:'#36402D',
+      white:'#ffffff'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          'hero': "url('../public/images/cafe_street.jpg')",
+          
+          
       },
+      opacity:{
+        '10':'0.1',
+        '20':'0.2',
+        '30':'0.3',
+        '40':'0.4',
+        '50':'0.5',
+        '60':'0.6',
+        '70':'0.7',
+        '78':'0.78',
+        '80':'0.8',
+        '90':'0.9',
+        '100':'1.0'
+      }
+      
     },
   },
   plugins: [require("daisyui")],

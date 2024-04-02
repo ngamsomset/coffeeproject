d50d27dacaf4bc2a4d9bf9e7dd4b9953350205c7
Brian Ngamsom <60306190+ngamsomset@users.noreply.github.com>
2024-04-02 18:14:45 +1100

    remove error code

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
-import { Button } from "@/app/components/ui/button";
+import { Button } from '@/app/components/ui/button';
 import {
   Form,
   FormControl,
@@ -13,22 +13,22 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/app/components/ui/form";
-import { Input } from "@/app/components/ui/input";
-import { toast } from "@/app/components/ui/use-toast";
+} from '@/app/components/ui/form';
+import { Input } from '@/app/components/ui/input';
+import { toast } from '@/app/components/ui/use-toast';
 
 const FormSchema = z.object({
   username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+    message: 'Username must be at least 2 characters.',
   }),
   password: z.string().min(6, {
-    message: "Password must be at least 6 characters.",
+    message: 'Password must be at least 6 characters.',
   }),
   fullname: z.string().min(1, {
-    message: "Fullname must be input",
+    message: 'Fullname must be input',
   }),
   birthday: z.string().min(6, {
-    message: "Password must be at least 6 characters.",
+    message: 'Password must be at least 6 characters.',
   }),
 });
 
@@ -38,49 +38,49 @@ export default function FormPage() {
   const form = useForm({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: "",
-      password: "",
-      fullname: "",
-      birthday: "",
+      username: '',
+      password: '',
+      fullname: '',
+      birthday: '',
     },
   });
 
   const onSubmit = async (data: FormData) => {
-    console.log("Submitting form", data);
+    console.log('Submitting form', data);
 
-    const { username: email, password,fullname,birthday } = data;
+    const { username: email, password, fullname, birthday } = data;
 
     try {
-      const response = await fetch("/api/auth/register", {
-        method: "POST",
+      const response = await fetch('/api/auth/register', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password,fullname,birthday }),
+        body: JSON.stringify({ email, password, fullname, birthday }),
       });
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        throw new Error('Network response was not ok');
       }
       // Process response here
-      console.log("Registration Successful", response);
-      toast({ title: "Registration Successful" });
+      console.log('Registration Successful', response);
+      toast({ title: 'Registration Successful' });
     } catch (error: any) {
-      console.error("Registration Failed:", error);
-      toast({ title: "Registration Failed", description: error.message });
+      console.error('Registration Failed:', error);
+      toast({ title: 'Registration Failed', description: error.message });
     }
   };
 
   return (
-    <Form {...form} className="w-2/3 space-y-6">
+    <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <FormField
           control={form.control}
-          name="username"
+          name='username'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="Username" {...field} />
+                <Input placeholder='Username' {...field} />
               </FormControl>
               <FormDescription>
                 This is your public display name.
@@ -90,42 +90,42 @@ export default function FormPage() {
         />
         <FormField
           control={form.control}
-          name="password"
+          name='password'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input placeholder="Password" {...field} type="password" />
+                <Input placeholder='Password' {...field} type='password' />
               </FormControl>
             </FormItem>
           )}
         />
         <FormField
           control={form.control}
-          name="fullname"
+          name='fullname'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Full Name</FormLabel>
               <FormControl>
-                <Input placeholder="Ex. John Doe" {...field} />
+                <Input placeholder='Ex. John Doe' {...field} />
               </FormControl>
             </FormItem>
           )}
         />
         <FormField
           control={form.control}
-          name="birthday"
+          name='birthday'
           render={({ field }) => (
             <FormItem>
               <FormLabel>Birthday</FormLabel>
               <FormControl>
-                <Input placeholder="" {...field} type="date" />
+                <Input placeholder='' {...field} type='date' />
               </FormControl>
             </FormItem>
           )}
         />
-        
-        <Button type="submit">Submit</Button>
+
+        <Button type='submit'>Submit</Button>
       </form>
     </Form>
   );

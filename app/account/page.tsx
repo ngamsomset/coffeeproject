341bc2f4d6a35b5f7e5725587996fc98e18050a7
sanderkg <sanderkvale@me.com>
2024-04-20 19:14:41 +1000

    Initial profile layout

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import PreferenceTile from '../components/PreferenceTile'
+import PersonalDetails from '../components/PersonalDetails'
+
 
 const page = () => {
   return (
-    <section className="mt-12 max-w-screen-lg mx-auto min-h-[60vh]">
-      <h1 className="text-2xl font-semibold text-center">My Profile</h1>
+    <section className="my-16 max-w-screen-lg mx-auto min-h-[60vh] px-8">
+      <div className='grid'>
+        <div className='grid grid-cols-3 gap-12 md:gap-4'>
+          <div className='col-span-3 md:col-span-2'>
+            <PersonalDetails />
+          </div>
+          <div className='col-span-3 md:col-span-1'>
+            <PreferenceTile />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 className='text-2xl'>Your latest café visits</h2>
+      </div>
     </section>
   )
 }

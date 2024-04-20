ee1dc563efb144a8b68dbf494c8b37d00e1097b3
Stampeiei <nont.simaraks@gmail.com>
2024-04-21 01:48:07 +1000

    Fix table name

--- app/api/auth/questionaire/route.ts
+++ app/api/auth/questionaire/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
 
 
     const response =
-      await sql`INSERT INTO testinguser (question1, question2, question3, question4, question5, question6, question7, question8, question9) VALUES (${question1}, ${question2},${question3},${question4},${question5},${question6},${question7},${question8},${question9})`;
+      await sql`INSERT INTO questionaire (question1, question2, question3, question4, question5, question6, question7, question8, question9) VALUES (${question1}, ${question2},${question3},${question4},${question5},${question6},${question7},${question8},${question9})`;
   } catch (e) {
     console.log({ e });
   }

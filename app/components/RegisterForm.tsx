f5b2a926e7b005f181221f79dfc61541ef7e9f06
Stampeiei <nont.simaraks@gmail.com>
2024-04-23 04:59:05 +1000

    Remove unused import

--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
-import { useRouter,usePathname, useSearchParams } from 'next/navigation';
+import { useRouter} from 'next/navigation';
 import { Button } from '@/app/components/ui/button';
 import {
   Form,

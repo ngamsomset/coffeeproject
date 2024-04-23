d0d401dcc97c8a9ac71670765a0f768873229c62
Stampeiei <nont.simaraks@gmail.com>
2024-04-23 15:49:53 +1000

    edit route to 7questions

--- app/api/auth/questionaire/route.ts
+++ app/api/auth/questionaire/route.ts
@@ -5,14 +5,14 @@ import { sql } from "@vercel/postgres";
 
 export async function POST(request: Request) {
   try {
-    const { question1, question2, question3, question4, question5, question6, question7, question8, question9, username } = await request.json();
+    const { question1, question2, question3, question4, question5, question6, question7, username } = await request.json();
 
 
-    console.log({ question1, question2, question3, question4, question5, question6, question7, question8, question9, username });
+    console.log({ question1, question2, question3, question4, question5, question6, question7,  username });
 
 
     const response =
-      await sql`INSERT INTO questionaire (question1, question2, question3, question4, question5, question6, question7, question8, question9,email) VALUES (${question1}, ${question2},${question3},${question4},${question5},${question6},${question7},${question8},${question9},${username})`;
+      await sql`INSERT INTO questionaire (question1, question2, question3, question4, question5, question6, question7,email) VALUES (${question1}, ${question2},${question3},${question4},${question5},${question6},${question7},${username})`;
   } catch (e) {
     console.log({ e });
   }
